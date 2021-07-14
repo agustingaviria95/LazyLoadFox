ae0ab181ec348ef62accb70374b05e9acf747acd
agustingaviria95 <agustin.gaviria@alteru.co>
2021-07-14 12:11:52 -0500

    first commit

---
+++ src/lazy.js
@@ -0,0 +1,21 @@
+
+const isIntersecting = (entry) => {
+    return entry.isIntersecting;
+};
+
+const loadImage = (entry) => {
+const container = entry.target;
+const imagen = container.firstChild;
+const url = imagen.dataset.src;
+imagen.src = url;
+//console.log(container.nodeName);
+observer.unobserve(container)
+};
+const observer = new IntersectionObserver((entries) => {
+entries
+.filter(isIntersecting)
+.forEach(loadImage);
+});
+export const registerImage = (imagen) => {
+observer.observe(imagen);
+};
